c91bc6859d903ecd147518a94a4dc8a41716769c
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-02-23 21:22:36 +0530

    update!

--- Backend1/Routes/workouts.js
+++ Backend1/Routes/workouts.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getWorkouts , getWorkout, postWorkout, deleteWorkout, updateWorkout} from "../Controller/workoutController.js";
+    import { getWorkouts , getWorkout, postWorkout, deleteWorkout, updateWorkout} from "../Controller/workoutController.js";
 const router = express.Router();
 
 
@@ -9,8 +9,8 @@ router.get("/:id", getWorkout )
 
 router.post("/", postWorkout)
 
-router.delete("/:id", deleteWorkout)
-
 router.patch("/:id", updateWorkout)
+
+router.delete("/:id", deleteWorkout)
 
 export default router;
