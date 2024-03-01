40f9cbb3f7e8b64e4ff971fb8de48d1d69df8be9
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-03-01 23:01:48 +0530

    added

---
+++ Frontend/src/Components/WorkoutDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function WorkoutDetail({workout}) {
+  return (
+    <div className='workout-details'>
+        <h2>{workout.title}</h2>
+        <p><strong>Reps :</strong> {workout.reps}</p>
+        <p><strong>Loads :</strong> {workout.reps}</p>
+        <p>{workout.createdAt}</p>
+    </div>
+  )
+}
+
+export default WorkoutDetail
