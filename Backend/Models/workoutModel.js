83d01c8786ea33ec12a036ff0de89b00345e7ba9
Deepak Dhakad <deepakdkd1188@gmail.com>
2024-05-27 12:15:56 +0530

    updated

--- Backend/Models/workoutModel.js
+++ Backend/Models/workoutModel.js
@@ -1,28 +1,28 @@
-import mongoose from "mongoose";
 if (typeof window === 'undefined') {
     const mongoose = require('mongoose');
   
-const Schema = mongoose?.Schema;
-
-const workoutSchema = new Schema({
-    title:{
-        type:String,
-        required : true
-    },
-    reps :{
-        type:Number,
-        required:true
-    },
-    loads:{
-        type:Number,    
-        required:true
-    }
-    ,
-    createdBy:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"user",
-        required:true
-    }
-}, { timestamps: true });
-
-export const  workout = mongoose.model( "Workout", workoutSchema);
+    const Schema = mongoose.Schema;
+  
+    const workoutSchema = new Schema({
+      title: {
+        type: String,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        required: true,
+      },
+      loads: {
+        type: Number,
+        required: true,
+      },
+      createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true,
+      },
+    }, { timestamps: true });
+  
+    module.exports = mongoose.model('Workout', workoutSchema);
+  }
+  
